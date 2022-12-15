9b4c10996fa7bd6cae8cc1b5a9f9c9810aaa6551
joan.sarola <joan.sarola@somenergia.coop>
2022-12-15 19:03:02 +0100

    Day 15 doing

--- day15/app15.py
+++ day15/app15.py
@@ -1,4 +1,4 @@
-file = 'test.txt'
+file = 'input.txt'
 logging = False
 
 fitxer = open(file, 'r')
@@ -7,26 +7,63 @@ files = fitxer.readlines()
 taula = {}
 
 def omplirManhattan(posX, posY, espai):
-    for fila in range(espai*-1, espai + 1):
-        for columna in range(espai*-1, espai + 1):            
-            if (abs(fila) + abs(columna)) <= espai:
+    global esquerra
+    global dreta
+    for columna in range(espai*-1, espai + 1):
+        for fila in range(espai*-1, espai + 1):
+            if (abs(columna) + abs(fila)) <= espai:
                 try:
-                    valor = taula[posX + fila, posY + columna]
+                    valor = taula[posX + columna, posY + fila]
                 except:
                     valor = 0
                 if valor == 0:
-                    taula[posX + fila, posY + columna] = '#'                    
+                    taula[posX + columna, posY + fila] = '#'
+                    if esquerra > (posX + columna):
+                        esquerra = posX + columna
+                    if dreta < (posX + columna):
+                        dreta = posX + columna
+
+esquerra = 10000000
+dreta = 0
 
 for indexFila, linea in enumerate(files):
+    print('Fila ', indexFila)
     cadena = linea.split()
     sensorX = int(cadena[2][:-1].split('=')[1])
     sensorY = int(cadena[3][:-1].split('=')[1])
     beaconX = int(cadena[8][:-1].split('=')[1])
     beaconY = int(cadena[9].split('=')[1])
-    print(sensorX,sensorY,beaconX,beaconY)
 
+    taula[sensorX, sensorY] = 'S'
+    taula[beaconX, beaconY] = 'B'
 
+    cols = [sensorX, beaconX]
+    colsSorted = sorted(cols, reverse = True)
+    # print(colsSorted)
+    fils = [sensorY, beaconY]
+    filsSorted = sorted(fils, reverse = True)
+    # print(filsSorted)
 
-omplirManhattan(5,2,3)
+    espaitmp = (colsSorted[0] - colsSorted[1]) + (filsSorted[0] - filsSorted[1])
+    
+    omplirManhattan(sensorX, sensorY, espaitmp)
 
-print(taula)
+# print(taula)
+print('Més esquerra ', esquerra)
+print('Més dreta ', dreta)
+
+fila = 10
+positions = 0
+for columna in range(esquerra, dreta + 1):            
+    try:
+        valor = taula[columna, fila]
+    except:
+        valor = 'X'
+    print(columna, '->', valor)
+    if valor == '#':
+        positions = positions + 1
+
+print('In the row where y=', fila, ', how many positions cannot contain a beacon? ', positions)
+
+        
+
