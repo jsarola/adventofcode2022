2bf93a1c4f22b87538ba9bcbc4725155408a6f67
Joan Sarola <jsarola@proton.me>
2023-10-22 10:34:59 +0200

    [REFACTOR] I don't remember what was doing

--- day20/app20.py
+++ day20/app20.py
@@ -17,31 +17,39 @@ if logging: print('LLista: ', llista, ' Llarg: ', llargLlista)
 llistaOriginal = llista.copy()
 
 for index, numero in enumerate(llistaOriginal):
-    posicio = llista.index(numero) + 1
+    posicio = llista.index(numero)
     
     if numero[1] != 0:
-        if numero[1] < 0 :
-            novaPosicio = (posicio + numero[1]) % llargLlista
+        llista.remove(numero)
+        if numero[1] < 0:
+            posicioa = (posicio + numero[1]) % llargLlista
+            posiciob = (posicio + numero[1] - 1) % llargLlista
+            print(numero[1], '-', posicioa, posiciob)
+        else:
+            posicioa = (posicio + numero[1]) % llargLlista
+            posiciob = (posicio + numero[1] + 1) % llargLlista
+            print(numero[1], '-', posicioa, posiciob)
+    
+        if (posicio + numero[1]) > llargLlista:
+            novaPosicio = (posicio + numero[1] + 1) % llargLlista
+        elif posicio + numero[1] <= 0:
+            novaPosicio = (posicio + numero[1] - 1) % llargLlista
         else:
             novaPosicio = (posicio + numero[1]) % llargLlista
-        llista.remove(numero)
-        llista.insert(novaPosicio + 1, numero)
-  
-    print(posicio, novaPosicio)
+        if logging: print(llista)
+        llista.insert(novaPosicio, numero)
 
-    
-    # if logging: print('LLista provisional: ', llista)
+  
+    #if logging: print('Pas ', index + 1, posicio, '-', novaPosicio, ' : ', llista)
 
 if logging: print('LLista final: ', llista)
 
 posicio0 = llista.index(numero0)
 
-print(posicio0)
-
 valor1000 = llista[(posicio0 + 1000) % (llargLlista)]
 valor2000 = llista[(posicio0 + 2000) % (llargLlista)]
 valor3000 = llista[(posicio0 + 3000) % (llargLlista)]
 
-print(valor1000, valor2000, valor3000)
+if logging: print(valor1000, valor2000, valor3000)
 
 print('What is the sum of the three numbers that form the grove coordinates? ', valor1000[1]+valor2000[1]+valor3000[1])
